1c882e3cfa8df7f899047717d932ac12ec67770b
pablo⚡️bacchetta <polbac@gmail.com>
2018-06-21 14:15:38 -0300

    change dist path

--- modules/angular/projects/troca-angular/copy-assets.js
+++ modules/angular/projects/troca-angular/copy-assets.js
@@ -1,6 +1,6 @@
 var copy = require('copy');
 
-copy('./src/lib/assets/*.*', '../../dist/troca-angular/assets', function(err, files) {
+copy('./src/lib/assets/*.*', '../../../../dist/angular/assets', function(err, files) {
     if (err) throw err;
     console.log('assets copy succesfull');
   });
