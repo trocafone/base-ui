1c077f275670d0e417b535b4a11d298a16a184b3
Eliana De Luca <elianadeluca@hotmail.com>
2018-06-22 11:54:16 -0300

    Remove Components

--- modules/angular/src/app/app.module.ts
+++ modules/angular/src/app/app.module.ts
@@ -7,13 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
-import { InputComponent } from '../../projects/troca-angular/src/lib/input/input.component';
-import { ButtonComponent } from '../../projects/troca-angular/src/lib/button/button.component';
-import { CheckboxComponent } from '../../projects/troca-angular/src/lib/checkbox/checkbox.component';
-import { LogoComponent } from '../../projects/troca-angular/src/lib/logo/logo.component';
-import { DropdownComponent } from '../../projects/troca-angular/src/lib/dropdown/dropdown.component';
-import { PickColorComponent } from '../../projects/troca-angular/src/lib/pick-color/pick-color.component';
-import { AlertComponent } from '../../projects/troca-angular/src/lib/alert/alert.component';
+
 
 @NgModule({
   declarations: [
