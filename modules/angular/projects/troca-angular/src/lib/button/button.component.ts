89301ff94900cba9906b75990fced4eaf835791b
pablo⚡️bacchetta <polbac@gmail.com>
2018-06-27 11:29:34 -0300

    button preloader

--- modules/angular/projects/troca-angular/src/lib/button/button.component.ts
+++ modules/angular/projects/troca-angular/src/lib/button/button.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { guid } from '../utils';
+import { constructDependencies } from '@angular/core/src/di/reflective_provider';
 
 @Component({
   selector: 'tf-button',
   templateUrl: './button.component.html',
   styleUrls: ['button.component.scss']
 })
-export class ButtonComponent implements OnInit {
-  @Input() translation:string;
-  @Input() isLoading?: boolean;
+export class ButtonComponent implements OnChanges {
+
+  public SPINNER_WIDTH = 24;
+
+  @Input() translation: string;
+  @Input() isLoading?: Boolean = false;
   @Input() className?: string;
+  @Input() loadingText?: string;
 
   @Output() onClick?: EventEmitter<any> = new EventEmitter<any>();
 
-  private INITIAL_CLASS = 'button-primary button-medium';
+  public offsetLeftPreloader: any = 0;
+
+  public guid: string = guid();
+
+  public INITIAL_CLASS = 'button-primary button-medium';
+
 
   constructor() {
     if (this.className === undefined) {
@@ -20,7 +31,31 @@ export class ButtonComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.setLoadingSize();
+  }
+
+  setLoadingSize() {
+    if (this.isLoading) {
+      const preloader = document.getElementById(`preloader-container-${this.guid}`);
+
+      if (preloader) {
+        const preloaderText = document.getElementById(`preloader-text-${this.guid}`);
+        const translation = document.getElementById(`translation-container-${this.guid}`);
+
+        preloaderText.style.display = 'inline-block';
+
+        const preloaderWidth = preloaderText.offsetWidth + this.SPINNER_WIDTH;
+        const translationWidth = translation.offsetWidth;
+
+        if (preloaderWidth > translationWidth) {
+          preloaderText.style.display = 'none';
+        } else {
+          preloaderText.style.display = 'inline-block';
+        }
+      }
+    }
+
   }
 
   onClickButton(e): void {
