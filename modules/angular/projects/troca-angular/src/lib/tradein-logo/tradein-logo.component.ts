85bd1d1e64c8388926b647782d086c46804c9794
Eliana De Luca <elianadeluca@hotmail.com>
2018-07-06 13:55:03 -0300

    Trade in logo light version

--- modules/angular/projects/troca-angular/src/lib/tradein-logo/tradein-logo.component.ts
+++ modules/angular/projects/troca-angular/src/lib/tradein-logo/tradein-logo.component.ts
@@ -1,6 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {InputType} from "../input/input.component";
 
 declare var require: any;
+
+export enum LogoTheme {
+  DEFAULT = 'default',
+  LIGHT = 'light'
+}
 
 @Component({
   selector: 'tf-tradein-logo',
@@ -11,8 +17,15 @@ export class TradeinLogoComponent implements OnInit {
 
   @Input() text?: string;
   @Input() link?: string;
+  @Input() url: string;
+  @Input() theme?: LogoTheme;
 
-  public url = require('../assets/trade-in.svg');
+  public getLogoTheme = (): string => {
+    if (this.theme === LogoTheme.LIGHT) {
+      return this.url = require('../assets/trade-in-light.svg');
+    }
+    return this.url = require('../assets/trade-in.svg');
+  }
 
   constructor() { }
 
